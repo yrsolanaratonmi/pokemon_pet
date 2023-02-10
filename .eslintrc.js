aed00c8a071162176bb6533aa9480449b6a1a854
yrsolanaratonmi <romacrying@icloud.com>
2023-02-10 11:49:50 +0300

    small fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,23 @@
 module.exports = {
   root: true,
-  overrides: [
-    {
-      files: ["*.ts"],
-      parserOptions: {
-        project: [
-          "tsconfig.*?.json",
-          "tsconfig.json"
-        ],
-        createDefaultProgram: true
-      },
-      extends: ["plugin:@angular-eslint/recommended", 'airbnb-typescript/base',  'prettier',
-        'plugin:prettier/recommended',
-        'plugin:import/recommended'],
-
+  overrides: [{
+    files: ["*.ts"],
+    parserOptions: {
+      project: ["tsconfig.*?.json", "tsconfig.json"],
+      createDefaultProgram: true
     },
-    {
-      files: ["*.component.html"],
-      extends: ["plugin:@angular-eslint/template/recommended"],
-      rules: {
-        "max-len": ["error", { "code": 140 }]
-      }
-    },
-    {
-      files: ["*.component.ts"],
-      extends: ["plugin:@angular-eslint/template/process-inline-templates"]
+    extends: ["plugin:@angular-eslint/recommended", 'airbnb-typescript/base', 'prettier', 'plugin:prettier/recommended', 'plugin:import/recommended']
+  }, {
+    files: ["*.component.html"],
+    extends: ["plugin:@angular-eslint/template/recommended"],
+    rules: {
+      "max-len": ["error", {
+        "code": 140
+      }]
     }
-  ]
-}
+  }, {
+    files: ["*.component.ts"],
+    extends: ["plugin:@angular-eslint/template/process-inline-templates"]
+  }],
+  extends: ["plugin:storybook/recommended"]
+};
