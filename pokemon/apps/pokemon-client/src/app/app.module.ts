c8f7f23ad80bb472ab582ea37497000d96210167
yrsolanaratonmi <romacrying@icloud.com>
2023-02-15 10:37:01 +0300

    переход на nx

---
+++ pokemon/apps/pokemon-client/src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { CarouselModule } from 'primeng/carousel';
+import { ChipModule } from 'primeng/chip';
+import { InputTextModule } from 'primeng/inputtext';
+import { PaginatorModule } from 'primeng/paginator';
+import { SkeletonModule } from 'primeng/skeleton';
+import { TagModule } from 'primeng/tag';
+import { AppComponent } from './app.component';
+import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
+import { CarouselComponent } from './components/pokemon-full/carousel/carousel.component';
+import { PokemonFullComponent } from './components/pokemon-full/pokemon-full.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { MultipleRenderDirective } from './directives/multiple-render.directive';
+import { FilterPokemonPipe } from './pipes/filter-pokemon.pipe';
+import { UpperCasePipe } from './pipes/upper-case.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PokemonListComponent,
+    PokemonCardComponent,
+    UpperCasePipe,
+    PokemonFullComponent,
+    CarouselComponent,
+    FilterPokemonPipe,
+    MultipleRenderDirective,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ButtonModule,
+    CardModule,
+    ChipModule,
+    PaginatorModule,
+    CarouselModule,
+    CommonModule,
+    InputTextModule,
+    TagModule,
+    SkeletonModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ pokemon/apps/pokemon-client/src/app/components/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,95 @@
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { fromEvent, map, Subject, tap } from 'rxjs';
+import { AllPokemonsData } from '../../dto/allPokemonsData.dto';
+import { SinglePokemonInfo } from '../../dto/singlePokemonInfo.dto';
+import { UnifiedResponse } from '../../dto/unifiedResponse.dto';
+import { PokemonService } from '../../services/pokemon.service';
+
+@Component({
+  selector: 'pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PokemonListComponent implements OnInit {
+  constructor(
+    public pokemonService: PokemonService,
+    private ref: ChangeDetectorRef,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  public pokemons: Array<SinglePokemonInfo> = [];
+
+  public startElement = 0;
+
+  public searchValue$: Subject<string> = new Subject();
+
+  public activePokemon = '';
+
+  public loader = false;
+
+  private constantPokemons: Array<SinglePokemonInfo> = [];
+
+  @Output() clickFunc = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    fromEvent(document.querySelector('input') as any, 'input').subscribe(
+      (event: any) => this.searchValue$.next(event.target.value)
+    );
+    this.searchValue$.subscribe((res) => this.filterPokemons(res));
+    this.setPokemons();
+  }
+
+  public paginate(event: any) {
+    this.startElement = event.first;
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+
+  public setIndex(name: string) {
+    this.clickFunc.emit(name);
+  }
+
+  public setActive(name: string) {
+    this.activePokemon = name;
+  }
+
+  private setPokemons() {
+    this.loader = true;
+    this.pokemonService
+      .getPokemons()
+      .pipe(
+        map((pokemonsData: AllPokemonsData) => {
+          pokemonsData.results.forEach((el: UnifiedResponse) => {
+            this.pokemonService
+              .getSinglePokemon(el.url)
+              .pipe(tap(() => this.ref.markForCheck()))
+              .subscribe((res) => {
+                this.pokemons.push(res);
+                this.constantPokemons.push(res);
+              });
+          });
+        })
+      )
+      .subscribe(() => (this.loader = false));
+  }
+
+  private filterPokemons(searchString: string) {
+    this.pokemons = this.constantPokemons;
+    this.loader = true;
+    this.pokemons = this.pokemons.filter((el) =>
+      el.name.includes(searchString)
+    );
+    this.loader = false;
+    this.cdr.detectChanges();
+  }
+}
