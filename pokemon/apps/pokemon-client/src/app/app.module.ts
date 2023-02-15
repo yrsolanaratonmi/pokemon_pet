ba308f1948294479768944f9da326fa6c198cd9c
yrsolanaratonmi <romacrying@icloud.com>
2023-02-15 10:47:16 +0300

    Revert "good refactor"
    This reverts commit 2f7e31d91f964275c43fcb6573b3af1383da8dcc.

--- pokemon/apps/pokemon-client/src/app/app.module.ts
+++ pokemon/apps/pokemon-client/src/app/app.module.ts
@@ -1,5 +1,10 @@
+<<<<<<< HEAD:pokemon/apps/pokemon-client/src/app/app.module.ts
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+=======
+/* eslint-disable import/no-extraneous-dependencies */
+import { CommonModule } from '@angular/common';
+>>>>>>> 27de4b3 (Revert "good refactor"):src/app/app.module.ts
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -12,10 +17,17 @@ import { PaginatorModule } from 'primeng/paginator';
 import { SkeletonModule } from 'primeng/skeleton';
 import { TagModule } from 'primeng/tag';
 import { AppComponent } from './app.component';
+<<<<<<< HEAD:pokemon/apps/pokemon-client/src/app/app.module.ts
 import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
 import { CarouselComponent } from './components/pokemon-full/carousel/carousel.component';
 import { PokemonFullComponent } from './components/pokemon-full/pokemon-full.component';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+=======
+import { CarouselComponent } from './components/pokemon-full/carousel/carousel.component';
+import { PokemonFullComponent } from './components/pokemon-full/pokemon-full.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { PokemonSearchComponent } from './components/pokemon-search/pokemon-search.component';
+>>>>>>> 27de4b3 (Revert "good refactor"):src/app/app.module.ts
 import { MultipleRenderDirective } from './directives/multiple-render.directive';
 import { FilterPokemonPipe } from './pipes/filter-pokemon.pipe';
 import { UpperCasePipe } from './pipes/upper-case.pipe';
