a9b9ab27c4203842b0f1f6971f0c8b1b664d7c2a
yrsolanaratonmi <romacrying@icloud.com>
2023-02-15 18:01:12 +0300

    //////

--- pokemon/apps/admin/src/app/app.module.ts
+++ pokemon/apps/admin/src/app/app.module.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { TabMenuModule } from 'primeng/tabmenu';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
+    TabMenuModule,
+    CommonModule,
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
